43b2b8edc199610e3b991fe3a9b3a18679daf2c2
nina shrestha <nina shrestha>
2016-07-01 18:37:34 -0500

    first

---
+++ std_mgmt/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace std_mgmt
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
